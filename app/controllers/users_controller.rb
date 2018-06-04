f9b6f7ec99dcf86c076b94d55f0e3b4b7813ed80
Hung Phan <hungpv@atmarkcafe.org>
2018-06-04 14:35:06 +0700

    add login, logout, register, edit profile user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,53 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  def index 
+    #@users = User.all
+    @users = User.paginate(page: params[:page], per_page: 5)
+  end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:success] = "Welcome #{@user.name.capitalize} to onlineShop app!"
+      redirect_to user_path(@user)
+    else 
+      render 'new'
+    end
+  end
+  
+  def show
+
+  end
+  
+  def edit
+  
+  end
+  
+  def update
+    if @user.update(user_params)
+      flash[:success] = "your account was updated successfully"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
