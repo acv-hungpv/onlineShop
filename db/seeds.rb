269c2ebcdf2673546d4342a6a76956f70ae4c728
Hung Phan <hungpv@atmarkcafe.org>
2018-06-04 16:04:01 +0700

    change seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'webhoseio-ruby'
+webhoseio = Webhoseio.new('344a242c-e0fc-4475-9b1d-6d1beb587589')
