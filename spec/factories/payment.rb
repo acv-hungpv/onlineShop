d872863dfff618f0db8cb5dde6d8f27a44ddcde8
Hung Phan <hungpv@atmarkcafe.org>
2018-06-27 08:57:52 +0700

    delete rspec

--- spec/factories/payment.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do  
-  factory :payment do 
-    phone_ship "01683853169"
-    name_ship "Hung"
-    address_ship "Tp.HCM"
-    response ({ 'PayerID' => 'i049290jfsjf' })
-  end
-end
